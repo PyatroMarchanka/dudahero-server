9d5e6c75bc590b48360f6a247d9c350a5fb03802
karotkavichy <karotkavichy@gmail.com>
2024-06-28 16:40:24 +0200

    add cors

--- config.ts
+++ config.ts
@@ -14,4 +14,5 @@ export const ENV = {
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   SESSION_SECRET: process.env.SESSION_SECRET,
+  FRONTEND_URL: process.env.FRONTEND_URL!,
 };
--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ import passport from "passport";
 import { authRouter } from "./src/auth/authRouter";
 import { useGoogleStrategy } from "./src/auth/passport.config";
 import { jwtAuth } from "./src/middleware/ jwtAuth";
+import { ENV } from "./config";
+import { userApi } from "./src/mongo/api/user";
+import cors from "cors";
 
 const app = express();
 const port = 8080;
@@ -12,22 +15,30 @@ useGoogleStrategy();
 
 app.use(
   session({
-    secret: process.env.SESSION_SECRET || "keyboard cat",
+    secret: ENV.SESSION_SECRET || "keyboard cat",
     resave: false,
     saveUninitialized: true,
   })
 );
 
+app.use(cors({ origin: "http://localhost:3000" }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("v1/auth", authRouter);
+app.use("/v1/auth", authRouter);
 
-app.get('/protected', (req, res) => {
-    console.log('Auth test')
+app.get("/v1/profile", async (req, res) => {
+  try {
     jwtAuth(req);
-    res.send('Auth works ! Hello World!')
-  })
+    const userId = req.header("userId");
+    const user = await userApi.getUserById(userId!);
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
