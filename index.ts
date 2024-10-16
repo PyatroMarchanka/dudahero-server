d43bb6d2c94186ca439331df5b5f319bb5882a03
karotkavichy <karotkavichy@gmail.com>
2024-10-16 19:22:36 +0200

    test

--- index.ts
+++ index.ts
@@ -29,15 +29,15 @@ app.use(
   } as CorsOptions)
 );
 
-app.use("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", ENV.FRONTEND_URL);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use("*", function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", ENV.FRONTEND_URL);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.use(passport.initialize());
 app.use(passport.session());
