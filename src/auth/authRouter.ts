6c1622f80d8470388c9711f9abcbe6c448d7a00f
karotkavichy <karotkavichy@gmail.com>
2024-11-15 20:13:05 +0100

    moved cors headera

--- src/auth/authRouter.ts
+++ src/auth/authRouter.ts
@@ -35,6 +35,18 @@ authRouter.post("/google-auth", async (req, res) => {
     }
 
     const token = jwt.sign({ user: req.user }, ENV.JWT_SECRET || "");
+
+    res.removeHeader("Access-Control-Allow-Origin")
+    res.setHeader("Access-Control-Allow-Origin", ENV.FRONTEND_URL);
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    
     res
       .status(200)
       .cookie("jwtToken", token, { httpOnly })
