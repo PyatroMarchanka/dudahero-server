7a29e42eb31c1a163bf7c38b690a229f7937ddfa
karotkavichy <karotkavichy@gmail.com>
2024-11-15 20:19:09 +0100

    remove headers

--- src/auth/authRouter.ts
+++ src/auth/authRouter.ts
@@ -36,15 +36,6 @@ authRouter.post("/google-auth", async (req, res) => {
 
     const token = jwt.sign({ user: req.user }, ENV.JWT_SECRET || "");
 
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    
     res
       .status(200)
       .cookie("jwtToken", token, { httpOnly })
