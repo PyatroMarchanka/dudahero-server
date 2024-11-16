baed9e7fcc381411739b4847beed46a3b3466b62
karotkavichy <karotkavichy@gmail.com>
2024-11-16 21:33:18 +0100

    update auth

--- src/middleware/jwtAuth.ts
+++ src/middleware/jwtAuth.ts
@@ -1,21 +1,18 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import { Request } from "express";
-import { ENV } from '../../config';
+import { ENV } from "../../config";
 
 export const jwtAuth = (req: Request) => {
-        const token = req.header('Authorization');
-        console.log('cookies', req.cookies)
-        if (!token) {
-            throw new Error("Authorization token is missing");
-        }
-        if (token.startsWith("Bearer ") == false){
-            throw new Error("Authorization token should start with Bearer");
-       } 
-       const jwtToken = token.substring(7, token.length);
-        try {
-            const decoded = jwt.verify(jwtToken, ENV.JWT_SECRET || '');
-            req.user = decoded;
-        } catch (err) {
-            throw new Error("Invalid token");
-        }
+  const jwtToken = req.cookies.jwtToken;
+  console.log("cookies", req.cookies.jwtToken);
+  if (!jwtToken) {
+    throw new Error("Authorization token is missing");
+  }
+
+  try {
+    const decoded = jwt.verify(jwtToken, ENV.JWT_SECRET || "");
+    req.user = decoded;
+  } catch (err) {
+    throw new Error("Invalid token");
+  }
 };
