6fac82cac125c68089489e026f9aed6229cf8e54
karotkavichy <karotkavichy@gmail.com>
2024-12-02 21:08:33 +0100

    fix cors put method

--- index.ts
+++ index.ts
@@ -53,10 +53,13 @@ app.use("/v1/auth", authRouter);
 app.use("/v1/songs", songRouter);
 app.use("/v1/admin", adminRouter);
 
-app.options('*', (req, res) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.status(200).end();
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
 });
 
 app.get("/v1/profile", async (req, res) => {
